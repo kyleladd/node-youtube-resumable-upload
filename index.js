c0f8796f6eaebb8a10dcd2613a5a1bc3d1721369
Kyle Ladd <kladd6@gmail.com>
2018-01-02 11:47:42 -0500

    Refresh Token

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var fs			= require('fs');
-var request		= require('request');
-var EventEmitter	= require('events').EventEmitter;
-var mime		= require('mime');
-var util		= require('util');
+var fs      = require('fs');
+var request   = require('request');
+var EventEmitter  = require('events').EventEmitter;
+var mime    = require('mime');
+var util    = require('util');
 //TODO-KL
 if (!String.prototype.startsWith) {
-	String.prototype.startsWith = function(search, pos) {
-		return this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search;
-	};
+  String.prototype.startsWith = function(search, pos) {
+    return this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search;
+  };
 }
 // https://stackoverflow.com/a/33159592/2000485
 Object.defineProperty(Object.prototype, "getProp", {
@@ -23,107 +23,129 @@ Object.defineProperty(Object.prototype, "getProp", {
     enumerable: false
 });
 function resumableUpload() {
-	this.byteCount	= 0; //init variables
-	this.tokens	= {};
+  this.byteCount  = 0; //init variables
+  this.tokens = {};
   this.file = '';
   this.size = 0;
   this.type = '';
-	this.metadata	= {};
-	this.retry	= -1;
-	this.host	= 'www.googleapis.com';
-	this.api	= '/upload/youtube/v3/videos';
+  this.metadata = {};
+  this.retry  = -1;
+  this.host = 'www.googleapis.com';
+  this.api  = '/upload/youtube/v3/videos';
 };
 
 util.inherits(resumableUpload, EventEmitter);
 
 //Init the upload by POSTing google for an upload URL (saved to self.location)
 resumableUpload.prototype.upload = function() {
-	var self = this;
-  self.getUploadInfo(function(){
-    var options = {
-      url:  'https://' + self.host + self.api + '?uploadType=resumable&part=snippet,status,contentDetails',
-      headers: {
-        'Host':     self.host,
-        'Authorization':    'Bearer ' + self.tokens.access_token,
-        'Content-Length':   new Buffer(JSON.stringify(self.metadata)).length,
-        'Content-Type':   'application/json',
-        'X-Upload-Content-Length':  self.size,
-        'X-Upload-Content-Type': self.type
-      },
-      body: JSON.stringify(self.metadata)
-    };
-    //Send request and start upload if success
+  var self = this;
+  self.getUploadInfo(self.startUpload);
+}
 
-    //TODO-KL: handle refesh token
-    request.post(options, function(err, res, body) {
-      if (err || !res.headers.location) {
-        self.emit('error', new Error(err));
-        self.emit('progress', 'Retrying ...');
-        if ((self.retry > 0) || (self.retry <= -1)) {
-          self.retry--;
-          self.upload(); // retry
-        } else {
-          return;
+resumableUpload.prototype.startUpload = function() {
+  var self = this;
+  var options = {
+    url:  'https://' + self.host + self.api + '?uploadType=resumable&part=snippet,status,contentDetails',
+    headers: {
+      'Host':     self.host,
+      'Authorization':    'Bearer ' + self.tokens.access_token,
+      'Content-Length':   new Buffer(JSON.stringify(self.metadata)).length,
+      'Content-Type':   'application/json',
+      'X-Upload-Content-Length':  self.size,
+      'X-Upload-Content-Type': self.type
+    },
+    body: JSON.stringify(self.metadata),
+    json:true
+  };
+  //Send request and start upload if success
+  request.post(options, function(err, res, body) {
+    if (err || !res.headers.location || res.statusCode === 401) {
+      self.emit('error', new Error((res.statusCode === 401 ? body.error.message : err)));
+      self.emit('progress', 'Retrying ...');
+      if ((self.retry > 0) || (self.retry <= -1)) {
+        self.retry--;
+        if(res.statusCode === 401){
+          self.refreshTokens(self.startUpload);
         }
+        else{
+          self.startUpload(); // retry
+        }
+      } else {
+        return;
       }
+    }
+    else{
       self.location = res.headers.location;
       self.send();
-    });
+    }
   });
+}
+
+resumableUpload.prototype.refreshTokens = function(callback){
+  var self = this;
+    var params = {
+      client_id: self.tokens.client_id,
+      client_secret: self.tokens.client_secret,
+      grant_type: "refresh_token",
+      refresh_token: self.tokens.refresh_token
+    };
+    request.post({url:"https://accounts.google.com/o/oauth2/token", form: params, json:true}, function(err, res, body) {
+      self.tokens.access_token = body.access_token;
+      callback.bind(self)();
+    });
 }
 
 resumableUpload.prototype.getUploadInfo = function(callback){
   var self = this;
-  if(self.file.startsWith('http://')||self.file.startsWith('https://')){
+  if(typeof self.file === 'string' && (self.file.startsWith('http://')||self.file.startsWith('https://'))){
     request.head({url:self.file},function(err,response,body){
       self.size = response.headers.getProp("Content-Length");
       self.type = response.headers.getProp("Content-Type");
-      callback();
+      callback.bind(self)();
     });
   }
   else {
-  	if(typeof self.file === 'string'){
-	    self.size = fs.statSync(self.file).size;
-	    self.type = mime.lookup(self.file);
-	  }
-	  callback();
-	}
+    if(typeof self.file === 'string'){
+      self.size = fs.statSync(self.file).size;
+      self.type = mime.lookup(self.file);
+    }
+    callback.bind(self)();
+  }
 }
 
 //Pipes uploadPipe to self.location (Google's Location header)
 resumableUpload.prototype.send = function() {
-	var self = this;
-	var options = {
-		url: self.location, //self.location becomes the Google-provided URL to PUT to
-		headers: {
-		  'Authorization':	'Bearer ' + self.tokens.access_token,
-		  'Content-Length': self.size - self.byteCount,
-		  'Content-Type':	self.type
-		}
-	}, uploadPipe;
-	try {
+  var self = this;
+  var options = {
+    url: self.location, //self.location becomes the Google-provided URL to PUT to
+    headers: {
+      'Authorization':  'Bearer ' + self.tokens.access_token,
+      'Content-Length': self.size - self.byteCount,
+      'Content-Type': self.type
+    }
+  }, uploadPipe;
+  try {
     //url path
-    if((self.file.startsWith('http://')||self.file.startsWith('https://'))){
-    	request.get(self.file).pipe(request.put(options, function(error, response, body) {
-			clearInterval(health);
-			if (!error) {
-				self.emit('success', body);
-				return;
-			}
-      //TODO-KL handle refresh token
-			self.emit('error', new Error(error));
-			if ((self.retry > 0) || (self.retry <= -1)) {
-				self.retry--;
-				self.getProgress(function(err, res, b) {
-					if (typeof res.headers.range !== 'undefined') {
-						self.byteCount = res.headers.range.substring(8); //parse response
-					} else {
-						self.byteCount = 0;
-					}
-					self.send();
-				});
-			}
-		}));
+    if(typeof self.file === 'string' && (self.file.startsWith('http://') || self.file.startsWith('https://'))){
+      request.get(self.file).pipe(request.put(options, function(error, response, body) {
+      clearInterval(health);
+      if (!error) {
+        self.emit('success', body);
+        return;
+      }
+      self.emit('error', new Error(error));
+      if ((self.retry > 0) || (self.retry <= -1)) {
+        self.retry--;
+        self.getProgress(function(err, res, b) {
+          if (typeof res.headers.range !== 'undefined') {
+            self.byteCount = res.headers.range.substring(8); //parse response
+          } else {
+            self.byteCount = 0;
+          }
+          self.send();
+        });
+      }
+    }));
     }
     else{
       // file path
@@ -163,25 +185,25 @@ resumableUpload.prototype.send = function() {
     return;
   }
   var health = setInterval(function(){
-		self.getProgress(function(err, res, body) {
-			if (!err && typeof res.headers.range !== 'undefined') {
-				self.emit('progress', res.headers.range.substring(8));
-			}
-		});
-	}, 5000);
+    self.getProgress(function(err, res, body) {
+      if (!err && typeof res.headers.range !== 'undefined') {
+        self.emit('progress', res.headers.range.substring(8));
+      }
+    });
+  }, 5000);
 }
 
 resumableUpload.prototype.getProgress = function(handler) {
-	var self = this;
-	var options = {
-		url: self.location,
-		headers: {
-		  'Authorization':	'Bearer ' + self.tokens.access_token,
-		  'Content-Length':	0,
-		  'Content-Range':	'bytes */' + self.size
-		}
-	};
-	request.put(options, handler);
+  var self = this;
+  var options = {
+    url: self.location,
+    headers: {
+      'Authorization':  'Bearer ' + self.tokens.access_token,
+      'Content-Length': 0,
+      'Content-Range':  'bytes */' + self.size
+    }
+  };
+  request.put(options, handler);
 }
 
 module.exports = resumableUpload;
